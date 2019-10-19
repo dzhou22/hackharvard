a0820f2d2ebaf8791dab60276408e60270c6ef36
dzhou22 <dylanzhou@college.harvard.edu>
2019-10-18 23:27:32 -0400

    logout

--- signup.php
+++ signup.php
@@ -6,6 +6,29 @@
 		<div>
 			<section>
                 <h1>Signup</h1>
+                <?php
+                    if (isset($_GET["error"])) {
+                        if ($_GET["error"] == "emptyfields") {
+                            echo '<p>Please fill in all the fields.</p>';
+                        } else if ($_GET['error'] == "invalidmailusername") {
+                            echo '<p>Invalid e-mail address and username. Username must use only alphanumeric characters.</p>';
+                        } else if ($_GET['error'] == "invalidmail") {
+                            echo '<p>Invalid e-mail.</p>';
+                        } else if ($_GET['error'] == "invalidusername") {
+                            echo '<p>Username must use only alphanumeric characters.</p>';
+                        } else if ($_GET['error'] == "passwordcheck") {
+                            echo '<p>Passwords do not match.</p>';
+                        } else if ($_GET['error'] == "invalidrole") {
+                            echo '<p>Please select your role.</p>';
+                        } else if ($_GET['error'] == "usernametaken") {
+                            echo '<p>Username is taken. Please choose another.</p>';
+                        }
+                    } else if (isset($_GET["signup"])) {
+                        if ($_GET["signup"] == "success") {
+                            echo '<p>Signup successful!</p>';
+                        }
+                    } 
+                ?>
 				<form action="includes/signup.inc.php" method = "post">
                     <input type="text" name="username" placeholder="Username">
                     <input type="text" name="mail" placeholder="E-mail">
