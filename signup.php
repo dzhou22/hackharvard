744de8722cefb08c16451ce24c3e18031a9f1e8b
Louis Golowich <lgolowich@college.harvard.edu>
2019-10-18 22:06:28 -0400

    Updated dropdown

--- signup.php
+++ signup.php
@@ -9,6 +9,12 @@
 				<form action="includes/signup.inc.php" method = "post">
                     <input type="text" name="username" placeholder="Username">
                     <input type="text" name="mail" placeholder="E-mail">
+					<select>
+						<option disabled selected value="none">- Select -</option>
+						<option value="student">Student</option>
+						<option value="tutor">Tutor</option>
+						<option value="Both">Both</option>
+					</select>
                     <input type="password" name="pwd" placeholder="Password">
                     <input type="password" name="pwd2" placeholder="Retype Password">
                     <button type="submit" name="signup-submit">Signup</button>
